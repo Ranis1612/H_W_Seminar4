07c54af8ee947a75130bba5f4a4537fc6458c1d0
Ranis1612 <Ranis1612@mail.ru>
2023-02-14 21:40:31 +0500

    Закоммитил домашку

---
+++ Z25/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);      
+Console.Write("Введите степень: ");
+int b = int.Parse(Console.ReadLine()!);       
+Console.Write($"Число {a} в степени {b} равно {GetXZFunction( b)} ");
+
+int GetXZFunction(int d)
+{
+  
+  int result = 1;                                                   
+for(int i = 0; i < b; i++)
+  { 
+  result = result * a;  
+  }
+return result;
+}
+
---
+++ Z27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!); 
+Console.Write($"Сумма цифр в числе {a} равна {GetSum( a)} ");
+
+int GetSum(int a)
+{
+  int sum = 0;
+int count = 0;
+if(a < 0)
+a *= (-1);
+while(a > 0)
+  {
+ count = a % 10;     
+//  Console.WriteLine(count);
+ sum += count;
+//  Console.WriteLine(sum);
+ a = a/10;
+//  Console.WriteLine(a); 
+  }
+return sum;
+}
